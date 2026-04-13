229abec6a191fd2d3f88ebb5037b12cc9b7bc4dc
Francisco Mossri <chicobulhoes@gmail.com>
2026-04-13 11:46:32 -0300

    chore: adiciona dep primeflex, styles e troca tema PrimeNG para Nora

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -3,11 +3,33 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeuix/themes/aura';
+import { MessageService } from 'primeng/api';
+import Nora from '@primeuix/themes/nora';
+import { definePreset, palette } from '@primeuix/themes';
 
 import { routes } from './app.routes';
 import { authInterceptor } from './core/auth/auth.interceptor';
 
+const AppPreset = definePreset(Nora, {
+    semantic: {
+      primary: palette('#3b5bdb'),
+      colorScheme: {
+        light: {
+            surface: {
+                0: '#e8eef5',
+            },
+            content: {
+                background: '#d3dde8',
+                hoverBackground: '#c5d0dc',
+            },
+            formField: {
+                background: '#e8eef5',
+                filledBackground: '#e8eef5',
+            },
+        },
+      },
+    },
+});
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true}),
@@ -17,9 +39,10 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     providePrimeNG({
         theme: {
-            preset: Aura,
+            preset: AppPreset,
             options: { darkModeSelector: false },
         },
     }),
+    MessageService,
   ],
 };
